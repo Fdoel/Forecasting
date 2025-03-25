39156294db1baad313a1b3f3c190cf081a345002
Maximilian Lopes Cardozo <662957ml@eur.nl>
2025-03-25 14:02:02 +0100

    allez

--- Basic AR and MAR.R
+++ Basic AR and MAR.R
@@ -5,3 +5,5 @@
 
 first_try <- marx(inflation_df_monthly$inflation, NULL, p_max = 10, sig_level = 0.1, p_C=0, p_NC = 6)
 second_try <- marx(inflation_df_monthly$inflation, NULL, p_max = 10, sig_level = 0.05, p_C = 3, p_NC = 3)
+
+ff_proberen <- marx(inflation_df_monthly$inflation, NULL, p_max = 15, sig_level = 0.05, p_C=3)
