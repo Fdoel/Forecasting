cd3880e3476e5a646f23319ff516f6559148fb77
Fdoel <vandendoel@ese.eur.nl>
2025-04-02 14:01:02 +0200

    d toegevoegd

--- MART.R
+++ MART.R
@@ -10,7 +10,7 @@
 #' @examples
 #' data <- sim.marx(c('t',3,1),c('t',1,1),100,0.5,0.4,0.3)
 #' regressor.matrix(data$y, data$x, 2)
-regressor.matrix_T <- function(y, x, p, c) {
+regressor.matrix_T <- function(y, x, p, c, d=1) {
   # Handle NULL x case
   if (missing(x) || is.null(x)) {
     x <- "not"
@@ -66,7 +66,7 @@ regressor.matrix_T <- function(y, x, p, c) {
       Z_x <- cbind(Z_x, Z_x)
       mX <- ncol(Z_x)
       for(i in 1:nT) {
-        if(y[p+i] > c) {
+        if(y[p+i-d] > c) {
           Z_x[i, 1:(mX/2)] <- 0
         } else {
           Z_x[i, (mX/2 + 1):mX] <- 0
@@ -75,7 +75,7 @@ regressor.matrix_T <- function(y, x, p, c) {
     }
     mC <- ncol(Z_c)
     for(i in 1:nT) {
-      if(y[p+i] > c) {
+      if(y[p+i-d] > c) {
         Z_c[i, 1:(mC/2)] <- 0
       } else {
         Z_c[i, (mC/2 + 1):mC] <- 0
@@ -114,7 +114,7 @@ regressor.matrix_T <- function(y, x, p, c) {
 #' data <- sim.marx(c('t',3,1),c('t',1,1),100,0.5,0.4,0.3)
 #' arx.ls(data$y,data$x,2)
 
-arx.ls_T <- function(y,x,p,c){
+arx.ls_T <- function(y,x,p,c,d=1){
   
   if (is.null(x)){
     x <- "not"
@@ -124,7 +124,7 @@ arx.ls_T <- function(y,x,p,c){
   
   Y <- y[(p+1):length(y)]
   int <- rep(1,(length(y)-p))
-  ZT <- regressor.matrix_T(y,x,p,c)
+  ZT <- regressor.matrix_T(y,x,p,c,d)
   ZT <- cbind(int,ZT)
   
   df <- nrow(ZT) - NCOL(ZT)
@@ -182,7 +182,7 @@ arx.ls_T <- function(y,x,p,c){
 }
 
 # Dit verwacht params in een bepaalde vorm die nu niet zo uit ARX_T komt
-ll.MART.Z <- function(params,y,x,p_C,p_NC,c){
+ll.MART.Z <- function(params,y,x,p_C,p_NC,c,d=1){
   p_CT <- p_C*2
   p_NCT <- p_NC*2
   if (is.null(x)){
@@ -255,7 +255,7 @@ ll.MART.Z <- function(params,y,x,p_C,p_NC,c){
   
   ZC1 <- y[(p_C+1):length(y)]
   ZC1 <- fBasics::vec(ZC1)
-  ZC2 <- regressor.matrix_T(y,"not",p_C, c)
+  ZC2 <- regressor.matrix_T(y,"not",p_C, c,d)
   
   if (p_C > 0){
     V <- ZC1 - (ZC2 %*% BC1)
@@ -267,9 +267,9 @@ ll.MART.Z <- function(params,y,x,p_C,p_NC,c){
   
   ZNC1 <- U[(p_NC + 1):length(U)]
   ZNC1 <- fBasics::vec(ZNC1)
-  ZNC2 <- regressor.matrix_T(U,"not",p_NC, c)
+  ZNC2 <- regressor.matrix_T(U,"not",p_NC, c,d)
   if((colnumT) > 1){
-    ZX <- regressor.matrix_T(y, x, 1, c)[, 3:(2+2*ncol(as.matrix(x)))]
+    ZX <- regressor.matrix_T(y, x, 1, c,d)[, 3:(2+2*ncol(as.matrix(x)))]
     for (i in 1:(colnumT)) {
       ZX[,i] <- rev(ZX[,i])
     }
@@ -306,7 +306,7 @@ ll.MART.Z <- function(params,y,x,p_C,p_NC,c){
 }
 
 # DEZE FUNCTIE MOET NOG AANGEPAST WORDEN
-MART <- function(y, x, p_C, p_NC, c) {
+MART <- function(y, x, p_C, p_NC, c, d=1) {
   p_CT <- 2*p_C
   p_NCT <- 2*p_NC
   nargin <- length(as.list(match.call())) - 2
@@ -337,9 +337,9 @@ MART <- function(y, x, p_C, p_NC, c) {
     z    <- rev(y)
     # Hier specificeer je startwaardes voor de parameters voor optimalisatie
     z    <- fBasics::vec(z) # Z hier is basically de toekomst
-    BC0  <- arx.ls_T(y,x,p_C,c)[[2]] # Fit een AR model en pak de phi's
-    Bx0  <- arx.ls_T(y,x,p_C,c)[[3]] # Fit een AR model en pak de beta's
-    BNC0 <- arx.ls_T(z,x.rev,p_NC,c)[[2]] # Fir een AR model op de omgedraaide volgorde, dus basically de toekomst
+    BC0  <- arx.ls_T(y,x,p_C,c,d)[[2]] # Fit een AR model en pak de phi's
+    Bx0  <- arx.ls_T(y,x,p_C,c,d)[[3]] # Fit een AR model en pak de beta's
+    BNC0 <- arx.ls_T(z,x.rev,p_NC,c,d)[[2]] # Fir een AR model op de omgedraaide volgorde, dus basically de toekomst
     IC0  <- 0
     df0  <- 20
     sig0 <- 2
@@ -376,7 +376,7 @@ MART <- function(y, x, p_C, p_NC, c) {
   
   if (length(x) > 1){
     numcol <- ncol(as.matrix(x))
-    ZX <- regressor.matrix_T(y, x, 1, c)[, 3:(2+2*ncol(as.matrix(x)))]
+    ZX <- regressor.matrix_T(y, x, 1, c,d)[, 3:(2+2*ncol(as.matrix(x)))]
     if (p_C > 0 && p_NC > 0){
       B_C  <- PARAMS[1:(p_CT)]
       B_NC <- PARAMS[(p_CT+1):(p_CT + p_NCT)]
@@ -441,7 +441,7 @@ MART <- function(y, x, p_C, p_NC, c) {
   
   ZC1 <- y[(p_C+1):length(y)]
   ZC1 <- fBasics::vec(ZC1)
-  ZC2 <- regressor.matrix_T(y,"not",p_C,c)
+  ZC2 <- regressor.matrix_T(y,"not",p_C,c,d)
   
   if (p_C > 0){
     V <- ZC1 - ZC2 %*% B_C
@@ -454,7 +454,7 @@ MART <- function(y, x, p_C, p_NC, c) {
   
   ZNC1 <- U[(p_NC + 1):length(U)]
   ZNC1 <- fBasics::vec(ZNC1)
-  ZNC2 <- regressor.matrix_T(U,"not",p_NC,c)
+  ZNC2 <- regressor.matrix_T(U,"not",p_NC,c,d)
   
   if(numcolT > 1){
     for (i in 1:numcolT){
@@ -514,7 +514,7 @@ logistic.smooth <- function(y, gamma, c) {
 #' @examples
 #' data <- sim.marx(c('t',3,1),c('t',1,1),100,0.5,0.4,0.3)
 #' regressor.matrix(data$y, data$x, 2)
-regressor.matrix_ST <- function(y, x, p, c, gamma) {
+regressor.matrix_ST <- function(y, x, p, c, gamma,d=1) {
   # Handle NULL x case
   if (missing(x) || is.null(x)) {
     x <- "not"
@@ -571,15 +571,15 @@ regressor.matrix_ST <- function(y, x, p, c, gamma) {
       mX <- ncol(Z_x)
       for(i in 1:nT) {
         Z_x_old <- Z_x[i,]
-        Z_x[i,1:(mX/2)] <- logistic.smooth(y[p+i], gamma, c)*Z_x_old[1:(mX/2)]
-        Z_x[i, (mX/2 + 1):mX] <- (1-logistic.smooth(y[p+i], gamma, c))*Z_x_old[(mX/2 + 1):mX]
+        Z_x[i,1:(mX/2)] <- logistic.smooth(y[p+i-d], gamma, c)*Z_x_old[1:(mX/2)]
+        Z_x[i, (mX/2 + 1):mX] <- (1-logistic.smooth(y[p+i-d], gamma, c))*Z_x_old[(mX/2 + 1):mX]
       }
     }
     mC <- ncol(Z_c)
     for(i in 1:nT) {
       Z_c_old <- Z_c[i,]
-      Z_c[i,1:(mC/2)] <- logistic.smooth(y[p+i], gamma, c)*Z_c_old[1:(mC/2)]
-      Z_c[i, (mC/2 + 1):mC] <- (1-logistic.smooth(y[p+i], gamma, c))*Z_c_old[(mC/2 + 1):mC]
+      Z_c[i,1:(mC/2)] <- logistic.smooth(y[p+i-d], gamma, c)*Z_c_old[1:(mC/2)]
+      Z_c[i, (mC/2 + 1):mC] <- (1-logistic.smooth(y[p+i-d], gamma, c))*Z_c_old[(mC/2 + 1):mC]
     }
     if (!identical(x, "not")) {
       ZT <- cbind(Z_c, Z_x)
@@ -614,7 +614,7 @@ regressor.matrix_ST <- function(y, x, p, c, gamma) {
 #' data <- sim.marx(c('t',3,1),c('t',1,1),100,0.5,0.4,0.3)
 #' arx.ls(data$y,data$x,2)
 
-arx.ls_ST <- function(y,x,p,c, gamma){
+arx.ls_ST <- function(y,x,p,c, gamma,d=1){
   
   if (is.null(x)){
     x <- "not"
@@ -624,7 +624,7 @@ arx.ls_ST <- function(y,x,p,c, gamma){
   
   Y <- y[(p+1):length(y)]
   int <- rep(1,(length(y)-p))
-  ZT <- regressor.matrix_ST(y,x,p,c,gamma)
+  ZT <- regressor.matrix_ST(y,x,p,c,gamma,d)
   ZT <- cbind(int,ZT)
   
   df <- nrow(ZT) - NCOL(ZT)
@@ -681,7 +681,7 @@ arx.ls_ST <- function(y,x,p,c, gamma){
   return(list(coefficients = B, coef.auto = B_auto, coef.exo = B_x, mse = Cov, residuals = U, loglikelihood = Loglik, fitted.values = FV, df = df,vcov=vcov))
 }
 
-ll.SMART.Z <- function(params,y,x,p_C,p_NC,c,gamma) {
+ll.SMART.Z <- function(params,y,x,p_C,p_NC,c,gamma,d=1) {
   p_CT <- p_C*2
   p_NCT <- p_NC*2
   if (is.null(x)){
@@ -754,7 +754,7 @@ ll.SMART.Z <- function(params,y,x,p_C,p_NC,c,gamma) {
   
   ZC1 <- y[(p_C+1):length(y)]
   ZC1 <- fBasics::vec(ZC1)
-  ZC2 <- regressor.matrix_ST(y,"not",p_C, c, gamma)
+  ZC2 <- regressor.matrix_ST(y,"not",p_C, c, gamma,d)
   
   if (p_C > 0){
     V <- ZC1 - (ZC2 %*% BC1)
@@ -766,9 +766,9 @@ ll.SMART.Z <- function(params,y,x,p_C,p_NC,c,gamma) {
   
   ZNC1 <- U[(p_NC + 1):length(U)]
   ZNC1 <- fBasics::vec(ZNC1)
-  ZNC2 <- regressor.matrix_ST(U,"not",p_NC, c, gamma)
+  ZNC2 <- regressor.matrix_ST(U,"not",p_NC, c, gamma,d)
   if((colnumT) > 1){
-    ZX <- regressor.matrix_ST(y, x, 1, c, gamma)[, 3:(2+2*ncol(as.matrix(x)))]
+    ZX <- regressor.matrix_ST(y, x, 1, c, gamma,d)[, 3:(2+2*ncol(as.matrix(x)))]
     for (i in 1:(colnumT)) {
       ZX[,i] <- rev(ZX[,i])
     }
@@ -805,7 +805,7 @@ ll.SMART.Z <- function(params,y,x,p_C,p_NC,c,gamma) {
 }
 
 
-SMART <- function(y, x, p_C, p_NC, c, gamma) {
+SMART <- function(y, x, p_C, p_NC, c, gamma,d=1) {
   p_CT <- 2*p_C
   p_NCT <- 2*p_NC
   nargin <- length(as.list(match.call())) - 2
@@ -875,7 +875,7 @@ SMART <- function(y, x, p_C, p_NC, c, gamma) {
   
   if (length(x) > 1){
     numcol <- ncol(as.matrix(x))
-    ZX <- regressor.matrix_ST(y, x, 1, c, gamma)[, 3:(2+2*ncol(as.matrix(x)))]
+    ZX <- regressor.matrix_ST(y, x, 1, c, gamma,d)[, 3:(2+2*ncol(as.matrix(x)))]
     if (p_C > 0 && p_NC > 0){
       B_C  <- PARAMS[1:(p_CT)]
       B_NC <- PARAMS[(p_CT+1):(p_CT + p_NCT)]
@@ -940,7 +940,7 @@ SMART <- function(y, x, p_C, p_NC, c, gamma) {
   
   ZC1 <- y[(p_C+1):length(y)]
   ZC1 <- fBasics::vec(ZC1)
-  ZC2 <- regressor.matrix_ST(y,"not",p_C,c, gamma)
+  ZC2 <- regressor.matrix_ST(y,"not",p_C,c, gamma,d)
   
   if (p_C > 0){
     V <- ZC1 - ZC2 %*% B_C
@@ -953,7 +953,7 @@ SMART <- function(y, x, p_C, p_NC, c, gamma) {
   
   ZNC1 <- U[(p_NC + 1):length(U)]
   ZNC1 <- fBasics::vec(ZNC1)
-  ZNC2 <- regressor.matrix_ST(U,"not",p_NC,c, gamma)
+  ZNC2 <- regressor.matrix_ST(U,"not",p_NC,c, gamma,d)
   
   if(numcolT > 1){
     for (i in 1:numcolT){
@@ -995,4 +995,190 @@ SMART <- function(y, x, p_C, p_NC, c, gamma) {
   se.dist <- rev(se.dist)
   
   return(list(coef.c1 = B_C[1:p_C], coef.c2 = B_C[(p_C+1):(p_CT)], coef.nc1 = B_NC[1:p_NC], coef.nc2 = B_NC[(p_NC+1):(p_NCT)], coef.exo1 = B_x[1:(length(B_x)/2)], coef.exo2 = B_x[(length(B_x)/2 +1): length(B_x)], coef.int = IC, scale = sig,df = df,residuals = E, se.dist = se.dist))
+}
+
+#' @title Forecasting function for the MART model
+#' @description   This function allows you to forecast with the mixed causal-noncausal model with possibly exogenous regressors.
+#' @param y       Data vector y.
+#' @param X       (optional) Matrix with data (column represent a series).
+#' @param p_C     Number of lags (causal order).
+#' @param p_NC    Number of leads (noncausal order).
+#' @param X.for   (optional) Matrix with forecasted values for X (column represents series).
+#' @param h       Forecast horizon h.
+#' @param M       (optional) Truncation value M for MA representation. Default value: 50.
+#' @param N       (optional) Number of simulations to forecast noncausal component. Default: 10,000.
+#' @param Seed    (optional) Seed for random number generation. Default: 9999
+#' @keywords forecasting
+#' @return \item{y.for}{Vector containing forecasted values for y.}
+#' @author Sean Telg
+#' @export
+#' @examples
+
+
+# Treshold verandert niet fundamenteel de simulatie van toekomstige errors, je moet alleen uitkijken dat dimensies enzo kloppen
+forecast.MART <- function(y,X,p_C,p_NC,c,X.for,h,M,N){
+  
+  set.seed(9999)
+  if (missing(X) == TRUE){
+    X = NULL
+  }
+  
+  if (missing(N) == TRUE){
+    N = 10000
+  }
+
+  object <- mixed(y,X,p_C,p_NC) # Dit aanpassen naar MART
+  obs <- length(y)
+  
+  ## Check whether there are exogenous variables and whether truncation M is known
+  
+  if (missing(X.for) == TRUE && missing(M) == TRUE){
+    X.for = NULL
+    M = 50
+  }
+  else if(missing(X.for) == TRUE && missing(M) == FALSE){
+    X.for = NULL
+    M = M
+  }
+  else if(missing(X.for) == FALSE && missing(M) == TRUE){
+    if (NCOL(X.for) == 1){
+      if(is.null(X.for) == TRUE){
+        M = 50
+      }
+      else{
+        M = length(X.for)
+      }
+    }
+    else{
+      M = length(X.for[,1])
+    }
+  } else if(missing(X.for) == FALSE && missing(M) == FALSE){
+    if (NCOL(X.for) == 1){
+      if(is.null(X.for) == TRUE){
+        M = M
+      }
+      else{
+        M = min(length(X.for), M)
+      }
+    }
+    else{
+      M = min(length(X.for[,1]),M)
+    }
+  }
+  
+  coef.caus <- c()
+  # Dit ff checken (dit klopt denk ik niet)
+  if (object$order[1] == 0){
+    r = 1
+    coef.caus <- object$coefficients[(r+1)]
+  }
+  else{
+    r = object$order[1]
+    coef.caus <- object$coefficients[2:(r+1)]
+  }
+  
+  coef.noncaus <- c()
+  if (object$order[2] == 0){
+    s = 1
+    coef.noncaus <- object$coefficients[(r+1+s)]
+  }
+  else{
+    s = object$order[2]
+    coef.noncaus <- object$coefficients[(r+2):(r+1+s)]
+  }
+  
+  coef.exo <- c()
+  if (object$order[3] == 0){
+    q = 1
+    coef.exo <- object$coefficients[(r+1+s+q)]
+  }
+  else{
+    q = object$order[3]
+    coef.exo <- object$coefficients[(r+1+s+1):(r+s+1+q)]
+  }
+  
+  ## Simulate future epsilon and use forecasted X
+  hve <- c()
+  hve2 <- matrix(data=0, nrow=N,ncol=h)
+  
+  for (iter in 1:N){
+    
+    eps.sim <- object$coefficients["scale",]*stats::rt(M,object$coefficients["df",])
+    
+    z2 <- c()
+    for (i in 1:M){
+      if(is.null(X.for) == TRUE){
+        z2[i] <- eps.sim[i]
+      }
+      else{
+        if(NCOL(X.for) > 1){
+          z2[i] <- eps.sim[i] +  coef.exo %*% t(X.for[i,])
+        }
+        else{
+          z2[i] <- eps.sim[i] + coef.exo * X.for[i]
+        }
+      }
+    }
+    
+    ## Compute filtered values u = phi(L)y and moving average values
+    # split in phi1 en phi2 voor twee regimes.
+    phi <- c(1,coef.caus)
+    
+    u <- c()
+    for (i in (r+1):obs){
+      # Hier de treshold inzetten
+      u[i] <- phi %*% y[i:(i-r)]
+    }
+    w <- c(u[(obs-s+1):obs],z2)
+    
+    C <- matrix(data=0, nrow=(M+s), ncol=(M+s))
+    C[1,] <- compute.MA(coef.noncaus,(M+s-1))
+    
+    if (s > 1){
+      for (i in 2:s){
+        C[i,] <- c(0, C[(i-1),1:(length(C[(i-1),])-1)])
+      }
+    }
+    
+    for (i in (s+1):(M+s)){
+      C[i,] <- c(rep(0,(i-1)),1,rep(0,(M+s-i)))
+    }
+    
+    D = solve(C)
+    
+    e <- D %*% w
+    
+    h1 <- c()
+    
+    for (i in 1:s){
+      h1[i] <- metRology::dt.scaled(e[i], df=object$coefficients["df",], sd=object$coefficients["scale",])
+    }
+    
+    hve[iter] = prod(h1)
+    
+    for (j in 1:h){
+      mov.av <-  C[1,1:(M-j+1)] %*% z2[j:M]
+      hve2[iter,j] <- mov.av * hve[iter]
+      
+    }
+  }
+  
+  y.star <- y[(obs-r+1):obs]
+  y.for <- c()
+  exp <- c()
+  
+  for (j in 1:h){
+    exp[j] = ((1/N)*sum(hve2[,j]))/((1/N)*sum(hve))
+    
+    if(length(coef.caus) == 1){
+      y.for[j] <-  object$coefficients[1]/(1-sum(coef.noncaus)) + coef.caus * y.star + exp[j]
+    }
+    else{
+      y.for[j] <-  object$coefficients[1]/(1-sum(coef.noncaus)) + t(coef.caus) %*% y.star + exp[j]
+    }
+    
+    y.star <- c(y.for[j], y.star[1:(length(y.star)-1)])
+  }
+  
+  return(y.for)
 }
