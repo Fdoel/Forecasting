cf7e1e7156449042e53609bfaf3077dd1870c1a1
Maximilian Lopes Cardozo <662957ml@eur.nl>
2025-04-03 13:36:48 +0200

    Update Basic AR and MAR.R

--- Basic AR and MAR.R
+++ Basic AR and MAR.R
@@ -4,8 +4,8 @@ source("MARX_functions.R")
 source("MART.R")
 
 # Loop over your values and capture the printed output
-p_C_max <- 12
-p_NC_max <- 12
+p_C_max <- 6
+p_NC_max <- 6
 
 AIC <- matrix(NA, nrow = p_C_max, ncol = p_NC_max)
 BIC <- matrix(NA, nrow = p_C_max, ncol = p_NC_max)
@@ -13,7 +13,7 @@ HQ <- matrix(NA, nrow = p_C_max, ncol = p_NC_max)
 
 for (i in 0:p_C_max) {
   for (j in 0:p_NC_max) {
-    marx_loop <- marx.t(inflation_df_monthly$inflationSA, NULL, p_C = i, p_NC = j)
+    marx_loop <- marx.t(inflation_df_monthly$inflationNonSA, NULL, p_C = i, p_NC = j)
     information <- information.criteria(type="MARX", marx_loop)
     AIC[i,j] <- information$aic
     BIC[i,j] <- information$bic
