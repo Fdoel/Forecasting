aea50bdcbc3c799d820925d84cef50da1f539b23
Maximilian Lopes Cardozo <662957ml@eur.nl>
2025-04-04 14:26:42 +0200

    Update Basic AR and MAR.R

--- Basic AR and MAR.R
+++ Basic AR and MAR.R
@@ -12,8 +12,19 @@ marx_test3 <- mixed(inflation_df_monthly$inflationNonSA, NULL, p_C=2, p_NC=2)
 marx_test4 <- mixed(inflation_df_monthly$inflationNonSA, NULL, p_C=3, p_NC=1)
 marx_test5 <- mixed(inflation_df_monthly$inflationNonSA, NULL, p_C=4, p_NC=0)
 
-forecast <- forecast.marx(y=inflation_df_monthly$inflationNonSA, X=NULL, p_C=1, p_NC=3, h=1, M=50, N=10000)$Y.for
+#forecasting test
+p <- 4
+M <- 50
+buffer <- 50
+forecasts_size <- length(inflation_df_monthly[,1]) - p - M - buffer
+forecast_vector <- vector(mode = "numeric", length = forecasts_size)
 
+for(i in 1:(forecasts_size-buffer)) {
+  forecast_data <- inflation_df_monthly[i:forecasts_size, ]
+  forecast_vector[i] <- forecast.marx(y=forecast_data$inflationNonSA, p_C=1, p_NC=3, h=1, M=M, N=100)
+}
+
+compare_data <- inflation_df_monthly[]
 
 
 
