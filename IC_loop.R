9483bacc14eb8de8ba986fa4de7841af6e0be069
Fdoel <vandendoel@ese.eur.nl>
2025-04-08 11:44:46 +0200

    Nieuw

--- IC_loop.R
+++ IC_loop.R
@@ -22,7 +22,7 @@ N <- matrix(NA, nrow = p_C_max + 1, ncol = p_NC_max + 1)
 for (i in 0:p_C_max) {
   
   for (j in 0:p_NC_max) {
-    marx_loop <- MART(inflation_df_monthly$inflationNonSA, NULL, p_C = i, p_NC = j, 1)
+    marx_loop <- MART(inflation_df_monthly$inflationNonSA, NULL, p_C = i, p_NC = j, 0.29)
     information <- information.criteria(type="MART", marx_loop)
     LL[(i+1),(j+1)] <- information$loglikelihood
     AIC[(i+1),(j+1)] <- information$aic
