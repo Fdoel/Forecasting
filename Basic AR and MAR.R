ea79ebcd06255c998229e6f7567fda01b18a3c7a
Maximilian Lopes Cardozo <662957ml@eur.nl>
2025-04-08 14:11:12 +0200

    -

--- Basic AR and MAR.R
+++ Basic AR and MAR.R
@@ -1,16 +1,33 @@
 # Load required library
 library(MASS)
 source("MARX_functions.R")
+source("MART.R")
+
+# Loop over your values and capture the printed output
+p_C_max <- 12
+p_NC_max <- 12
+
+LL <- matrix(NA, nrow = p_C_max + 1, ncol = p_NC_max + 1)
+AIC <- matrix(NA, nrow = p_C_max + 1, ncol = p_NC_max + 1)
+BIC <- matrix(NA, nrow = p_C_max + 1, ncol = p_NC_max + 1)
+HQ <- matrix(NA, nrow = p_C_max + 1, ncol = p_NC_max + 1)
+N <- matrix(NA, nrow = p_C_max + 1, ncol = p_NC_max + 1)
+
+for (i in 0:p_C_max) {
+  for (j in 0:p_NC_max) {
+    marx_loop <- marx.t(inflation_df_monthly$inflationNonSA, NULL, p_C = i, p_NC = j)
+    information <- information.criteria(type="MARX", marx_loop)
+    LL[(i+1),(j+1)] <- information$loglikelihood
+    AIC[(i+1),(j+1)] <- information$aic
+    BIC[(i+1),(j+1)] <- information$bic
+    HQ[(i+1),(j+1)] <- information$hq
+    N[(i+1),(j+1)] <- information$n
+  }
+}
 
 
-#gwn interface volgen
-marx(inflation_df_monthly$inflationNonSA, NULL, p_max=6, sig_level = 0.1)
 
-marx_test1 <- mixed(inflation_df_monthly$inflationNonSA, NULL, p_C=0, p_NC=4)
-marx_test2 <- mixed(inflation_df_monthly$inflationNonSA, NULL, p_C=1, p_NC=3)
-marx_test3 <- mixed(inflation_df_monthly$inflationNonSA, NULL, p_C=2, p_NC=2)
-marx_test4 <- mixed(inflation_df_monthly$inflationNonSA, NULL, p_C=3, p_NC=1)
-marx_test5 <- mixed(inflation_df_monthly$inflationNonSA, NULL, p_C=4, p_NC=0)
+
 
 #forecasting test
 p <- 4
