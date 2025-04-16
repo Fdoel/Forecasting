69b01aa6cc3368ebe4fb71c42edb98895935cc5b
Maximilian Lopes Cardozo <662957ml@eur.nl>
2025-04-16 17:38:17 +0200

    Update AR-X and MAR-X.R

--- AR-X and MAR-X.R
+++ AR-X and MAR-X.R
@@ -21,10 +21,10 @@ library(stats)
 # Model order selection for MARX model using information criteria
 # -----------------------------------------------------------------------------
 
-external_reg <- cbind(GS1, CUMFNS, IPFINAL)
+external_reg <- cbind(inflation_df_monthly$GS1, inflation_df_monthly$IPFINAL, inflation_df_monthly$CUMFNS)
 
 # Initial model diagnostic: scan up to lag 18 with 10% significance level
-marx(inflation_df_monthly$inflationNonSA, external_reg, p_max = 18, sig_level = 0.1)
+#marx(inflation_df_monthly$inflationNonSA, external_reg, p_max = 18, sig_level = 0.1)
 
 # Define maximum lag orders for causal (p_C) and noncausal (p_NC) components
 p_C_max <- 12
@@ -40,7 +40,7 @@ N <- matrix(NA, nrow = p_C_max + 1, ncol = p_NC_max + 1)
 # Loop over all possible (p_C, p_NC) combinations and compute info criteria
 for (i in 0:p_C_max) {
   for (j in 0:p_NC_max) {
-    marx_loop <- marx.t(inflation_df_monthly$inflationNonSA, NULL, p_C = i, p_NC = j)
+    marx_loop <- marx.t(inflation_df_monthly$inflationNonSA, external_reg, p_C = i, p_NC = j)
     information <- information.criteria(type = "MARX", marx_loop)
     LL[(i+1),(j+1)] <- information$loglikelihood
     AIC[(i+1),(j+1)] <- information$aic
