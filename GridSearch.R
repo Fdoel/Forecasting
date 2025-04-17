f3d7acdabca2b7c1bfdcefd0ba8b379d13e5a23c
ImmeSchraag <658072is@eur.nl>
2025-04-17 13:56:04 +0200

    sum

--- GridSearch.R
+++ GridSearch.R
@@ -8,7 +8,7 @@ load("inflation_df_monthly.RData")
 thresholds <- seq(0.1, 0.6, by = 0.1) #0.6 # median(inflation_df_monthly$inflationNonSA, na.rm = TRUE)
 ds <- seq(1, 6)
 gammas <- seq(1,10)
-p_C_max <- 2
+p_C_max <- 6
 p_NC_max <- 2
 
 # Set number of cores based on OS
@@ -21,23 +21,25 @@ if (.Platform$OS.type == "windows") {
 
 # Create a parameter grid of all combinations
 param_grid <- expand.grid(
-  x = exo_regres,
   threshold = thresholds,
+  x = NULL,
+  y = inflation_df_monthly$inflationNonSA,
   d = ds,
   i = 0:p_C_max, 
-  j = 0:p_NC_max
+  j = 0
 )
 
 # Function to run MART and get BIC value
 run_model <- function(params) {
   t <- params$threshold
+  y <- params$y
   x <- params$x
   d <- params$d
   i <- params$i
   j <- params$j
   
   # Run MART model
-  MART_d <- MART(inflation_df_monthly$inflationNonSA,x, i, j, t, d)
+  MART_d <- MART(y, x, i, j, t, d)
   bic_value <- information.criteria("MART", MART_d)
   return(data.frame(threshold = t, d = d, i = i, j = j, bic = bic_value))
 }
@@ -50,11 +52,12 @@ bic_results <- pbmclapply(
 )
 
 # Combine all results into one data frame
-bic_mart_d_df <- do.call(rbind, bic_results)
+bic_art_x_t_d_df <- do.call(rbind, bic_results)
 
 # Save the results
-save(bic_mart_d_df, file = "bic_mart_t_d_i_j_.RData")
+save(bic_art_x_t_d_df, file = "bic_art_x_t_d_df.RData")
 
-View(bic_mart_d_df)
+View(bic_art_x_t_d_df)
+
 
 
