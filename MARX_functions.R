34b876e19cf65da0f344a4444b9b8f61cbfeb2f0
Fdoel <vandendoel@ese.eur.nl>
2025-04-20 17:54:25 +0200

    Made marx return all standard errors

--- MARX_functions.R
+++ MARX_functions.R
@@ -551,7 +551,6 @@ marx.t <- function(y,x,p_C,p_NC,params0){
   }
 
   se <- sqrt(diag(solve(optimization_results$hessian)))
-  se.dist <- se[(length(se)-1):length(se)]
   se.dist <- rev(se.dist)
 
   return(list(coef.c = B_C, coef.nc = B_NC, coef.exo = B_x, coef.int = IC, scale = sig,df = df,residuals = E, se.dist = se.dist))
