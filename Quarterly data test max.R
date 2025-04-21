5a76a9cd608c28400ac61d758652048777157736
ImmeSchraag <658072is@eur.nl>
2025-04-21 10:06:04 +0200

    Update Quarterly data test max.R

--- Quarterly data test max.R
+++ Quarterly data test max.R
@@ -185,3 +185,133 @@ rmse <- function(forecast, actual) {
 
 rmse_mixed <- rmse(forecast_mixed, actual_matrix)
 rmse_causal <- rmse(forecast_causal, actual_matrix)
+
+# -----------------------------------------------------------------------------
+# Grid search SETAR
+# -----------------------------------------------------------------------------
+
+library(pbmcapply)
+source("MART.R")
+
+# Set model parameters
+thresholds <- seq(0.1, 0.6, by = 0.1) 
+ds <- seq(1, 6)
+p_C_max <- 6
+p_NC_max <- 6
+
+# Set number of cores based on OS
+if (.Platform$OS.type == "windows") {
+  n_cores <- 1
+} else {
+  n_cores <- 3 
+  RNGkind("L'Ecuyer-CMRG")  # Safe parallel RNG
+}
+
+# Create a parameter grid of all combinations
+param_grid <- expand.grid(
+  threshold = thresholds,
+  d = ds,
+  #g = gammas,
+  i = 0:p_C_max, 
+  j = 0
+)
+
+# Function to run MART and get BIC value
+run_model <- function(params) {
+  t <- params$threshold
+  d <- params$d
+  #gamma <- params$g
+  i <- params$i
+  j <- params$j
+  
+  # Run MART model
+  MART_d <- MART(inflation_df_quarterly$infltionNonSA, NULL, i, j, t, d)
+  bic_value <- information.criteria("MART", MART_d)
+  return(data.frame(threshold=t, d=d, i=i, j=j, bic=bic_value))
+}
+
+# Use pbmclapply with a progress bar
+bic_results <- pbmclapply(
+  1:nrow(param_grid),
+  function(idx) run_model(param_grid[idx, ]),
+  mc.cores = n_cores
+)
+
+# Combine all results into one data frame
+ART_Q <- do.call(rbind, bic_results)
+
+# Voeg de gamma-kolom toe aan het uiteindelijke data frame
+#bic_smart_tdig_df$gamma <- param_grid$g
+
+
+# Save the results
+save(ART_Q, file = "BIC_ART_Q.RData")
+
+View(ART_Q)
+
+
+# -----------------------------------------------------------------------------
+# Grid search MART
+# -----------------------------------------------------------------------------
+
+library(pbmcapply)
+source("MART.R")
+
+# Set model parameters
+thresholds <- seq(0.1, 0.6, by = 0.1) 
+ds <- seq(1, 6)
+p_C_max <- 6
+p_NC_max <- 6
+
+# Set number of cores based on OS
+if (.Platform$OS.type == "windows") {
+  n_cores <- 1
+} else {
+  n_cores <- 3 
+  RNGkind("L'Ecuyer-CMRG")  # Safe parallel RNG
+}
+
+# Create a parameter grid of all combinations
+param_grid <- expand.grid(
+  threshold = thresholds,
+  d = ds,
+  #g = gammas,
+  i = 0:p_C_max, 
+  j = 0:p_NC_max
+)
+
+# Function to run MART and get BIC value
+run_model <- function(params) {
+  t <- params$threshold
+  d <- params$d
+  #gamma <- params$g
+  i <- params$i
+  j <- params$j
+  
+  # Run MART model
+  MART_d <- MART(inflation_df_quarterly$infltionNonSA, NULL, i, j, t, d)
+  bic_value <- information.criteria("MART", MART_d)
+  return(data.frame(threshold=t, d=d, i=i, j=j, bic = bic_value))
+}
+
+# Use pbmclapply with a progress bar
+bic_results <- pbmclapply(
+  1:nrow(param_grid),
+  function(idx) run_model(param_grid[idx, ]),
+  mc.cores = n_cores
+)
+
+# Combine all results into one data frame
+MART_Q <- do.call(rbind, bic_results)
+
+# Voeg de gamma-kolom toe aan het uiteindelijke data frame
+#bic_smart_tdig_df$gamma <- param_grid$g
+
+
+# Save the results
+save(MART_Q, file = "BIC_MART_Q.RData")
+
+View(MART_Q)
+
+
+
