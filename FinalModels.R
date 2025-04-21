b91a3dd5456839f4f29573b86998218fdacf3b40
Fdoel <vandendoel@ese.eur.nl>
2025-04-21 14:20:56 +0200

    Wrong models

--- FinalModels.R
+++ FinalModels.R
@@ -13,12 +13,12 @@ AR <- arx.ls(inflation_df_monthly$inflationNonSA,NULL,12)
 AR_x <- arx.ls(inflation_df_monthly$inflationNonSA, exo, 12)
 
 # SETAR models
-SETAR <- arx.ls_T(inflation_df_monthly$inflationNonSA, NULL, 2, 0.6,1) # SETAR using grid search
-SETAR_x <- arx.ls_T(inflation_df_monthly$inflationNonSA, NULL, 2, 0.3 ,1) # SETAR-X using grid search
+SETAR <- MART(inflation_df_monthly$inflationNonSA, NULL, 2, 0, 0.6,1) # SETAR using grid search
+SETAR_x <- MART(inflation_df_monthly$inflationNonSA, exo, 2, 0, 0.3 ,1) # SETAR-X using grid search
 
 # STAR models
-STAR <- arx.ls_ST(inflation_df_monthly$inflationNonSA, NULL, 2, 0.6,1,1) # STAR using grid search
-STAR_x <- arx.ls_ST(inflation_df_monthly$inflationNonSA, NULL, 2, 0.3 ,1,1) # STAR-X using grid search
+STAR <- SMART(inflation_df_monthly$inflationNonSA, NULL, 2,0, 0.6,1,1) # STAR using grid search
+STAR_x <- SMART(inflation_df_monthly$inflationNonSA, exo, 2,0, 0.3,1,1) # STAR-X using grid search
 
 # All MAR models
 MAR_grid_pseudo <- marx.t(inflation_df_monthly$inflationNonSA, NULL, 1,12) # MAR is the same for grid search and pseudo
